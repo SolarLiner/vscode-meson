0fc00bc26a3d6e07d8f27bb045266170c58dda3a
Nathan Graule <solarliner@gmail.com>
2019-10-29 14:05:47 +0100

    Fix treeview duplicates having same ID

--- src/treeview/basenode.ts
+++ src/treeview/basenode.ts
@@ -1,14 +1,23 @@
 import * as vscode from "vscode";
+import { createHash } from "crypto";
 
 export class BaseNode {
-  constructor(public readonly id: string) {}
+  static count: number = 0;
+  public readonly id: string;
+  constructor(public readonly label: string) {
+    const hash = createHash("sha1");
+    hash.update(label);
+    hash.update((BaseNode.count++).toFixed());
+    this.id = hash.digest("base64");
+    hash.destroy();
+  }
   getChildren(): vscode.ProviderResult<BaseNode[]> {
     return [];
   }
   getTreeItem(): vscode.ProviderResult<vscode.TreeItem> {
     const item = new vscode.TreeItem(this.id);
     item.id = this.id;
-    item.label = this.id;
+    item.label = this.label;
     return item;
   }
 }
